64406ec083e81932ecd07f3567903e7bfdcbc60c
lekojson <jesper.j.tornros@gmail.com>
2019-09-16 08:53:16 +0200

    escape param

--- travelplanner/reconstruction.go
+++ travelplanner/reconstruction.go
@@ -1,7 +1,11 @@
 package travelplanner
 
+import (
+	"net/url"
+)
+
 func (c *Client) Reconstruction(ctx string) (*TripResp, error) {
-	path := "/api2/travelplannerv3_1/reconstruction.xml?ctx=" + ctx
+	path := "/api2/travelplannerv3_1/reconstruction.xml?ctx=" + url.QueryEscape(ctx)
 	req, err := c.NewRequest("GET", path, nil)
 	if err != nil {
 		return nil, err
