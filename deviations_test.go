25ecc6fa0ad5f1e359a4bcc5299f959186d1f7a7
lekojson <lekojson@users.noreply.github.com>
2020-02-09 10:54:36 +0100

    add time convert test

--- deviations_test.go
+++ deviations_test.go
@@ -21,3 +21,24 @@ func TestDeviations(t *testing.T) {
 		t.Errorf("caught unexpected error, %v", err)
 	}
 }
+
+func TestDeviationsTimeConvert(t *testing.T) {
+	resp, err := client.Deviations(&DeviationsRequest{
+		SiteID: "9192",
+	})
+	if err != nil {
+		t.Errorf("caught unexpected error, %v", err)
+	}
+
+	for _, d := range resp.Data {
+		if _, err := d.FromDate(); err != nil {
+			t.Errorf("error caught converting from date, %v", err)
+		}
+		if _, err := d.ToDate(); err != nil {
+			t.Errorf("error caught converting to date, %v", err)
+		}
+		if _, err := d.UpdatedDate(); err != nil {
+			t.Errorf("error caught converting updated date, %v", err)
+		}
+	}
+}
